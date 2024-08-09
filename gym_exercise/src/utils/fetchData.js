c79120edef776100b971ba8fb4c373fd5a2ef7a7
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-08-09 17:37:47 +0700

    fix: resolve API data issue in ReactJS Adjusted GET request configuration and data handling to ensure all data from the API is displayed correctly.
    update: add eslint to project
    Configured ESLint for code quality and consistency across the project.

--- gym_exercise/src/utils/fetchData.js
+++ gym_exercise/src/utils/fetchData.js
@@ -1,22 +1,22 @@
 export const exerciseOptions = {
-    method: "GET",
-    headers: {
-        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    },
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
 };
 
 export const youtubeOptions = {
-    method: "GET",
-    headers: {
-        "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-        "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
-    },
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '373a89205fmsh3fa99c1506d8efbp1c6f46jsnae9c3ee4b1a4',
+  },
 };
 
 export const fetchData = async (url, options) => {
-    const res = await fetch(url, options);
-    const data = await res.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
-    return data;
+  return data;
 };
